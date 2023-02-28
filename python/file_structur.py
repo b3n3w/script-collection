878237f71d6db3a614c8966b2cd33618902e4303
WagnerB <benedikt.wagner@smartstreaminnovations.com>
2023-02-28 08:48:04 +0100

    update extensions

--- python/file_structur.py
+++ python/file_structur.py
@@ -5,15 +5,16 @@ from os import path
 
 # Define file endings here
 DIRETORIES = [
-    {"name": 'Documents and Text', "endings": [
-        '.pdf', '.txt', '.doc', '.docx', '.fsxml', '.xml']},
-    {"name": 'Music', "endings": ['.mp3', '.flak']},
-    {"name": 'Images', "endings": ['.jpeg', '.jpg', '.png', '.svg', '.HEIF']},
-    {"name": 'Design', "endings": ['.afpub', '.afdesign', '.psd']},
-    {"name": 'Videos', "endings": ['.mkv', '.mpeg', '.avi', '.mp4']},
-    {"name": 'Other', "endings": ['.nzb']},
-    {"name": 'Compressed', "endings": ['.rar', '.zip', '.7z']},
-    {"name": 'Books', "endings": ['.epub', '.mobi']}
+    {"name": '1_Documents', "endings": [
+        '.pdf', '.txt', '.doc', '.docx', '.fsxml', '.xml', '.csv', '.xlsx']},
+    {"name": '2_Coding', "endings": ['.css', '.js', '.ts', '.json', '.sh', '.bat', '.jar', '.java', '.py', '.yaml', '.yml', '.html' ]},
+    {"name": '3_Archives', "endings": ['.rar', '.zip', '.7z', '.tar', '.tar.gz','.bz2','.tgz','.gz']},
+    {"name": '4_Images', "endings": ['.jpeg', '.jpg', '.png', '.svg', '.HEIF', '.webp']},
+    {"name": '5_Videos', "endings": ['.mkv', '.mpeg', '.avi', '.mp4']},
+    {"name": '6_Music', "endings": ['.mp3', '.flak']},
+    {"name": '7_Design', "endings": ['.afpub', '.afdesign', '.psd']},
+    {"name": '8_Books', "endings": ['.epub', '.mobi']},
+    {"name": '9_Other', "endings": ['.nzb', '.log']}
 ]
 
 
@@ -24,9 +25,12 @@ def check_directories(folder):
         print('given folder does not exists')
         sys.exit()
     for directory in DIRETORIES:
-        if not path.exists(f'{folder}/{directory["name"]}'):
-          dir = path.join(folder, directory["name"])
-          os.mkdir(dir)
+          for filename in os.listdir(folder):
+               file = os.path.join(folder, filename)
+               if file.lower().endswith(tuple((directory['endings']))):
+                    if not path.exists(f'{folder}/{directory["name"]}'):
+                        dir = path.join(folder, directory["name"])
+                        os.mkdir(dir)
 
 def sort_files(folder):
     for filename in os.listdir(folder):
@@ -47,8 +51,10 @@ def main(argv):
         elif opt in ("-f", "--folder"):
             ROOTFOLER = arg
     print('Structuring folder: ', ROOTFOLER)
+
     check_directories(ROOTFOLER)
     sort_files(ROOTFOLER)
+    
     print("DONE")
 
 
